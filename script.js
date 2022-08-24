18708c465f1e2ad026410d966e70f39a9aabace7
Kabir Olawore <horlaworey@gmail.com>
2022-08-24 16:45:31 +0100

    Added DOM traversing

--- script.js
+++ script.js
@@ -80,7 +80,39 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-// // Lectures //
+///////////////// DOM the Traversing ///////////////
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child.
+// querySelector also works on elements
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); //This is better than childNodes
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'rebeccapurple';
+
+// Going upwards
+console.log(h1.parentElement);
+console.log(h1.parentNode);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+});
+
+// // Lectures /////
 
 // // console.log(document.getElementsByClassName('btn'));
 // const message = document.createElement('div');
