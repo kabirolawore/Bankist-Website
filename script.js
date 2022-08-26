0ba55775805d3790676fecfcc40435edcb019c9c
Kabir Olawore <horlaworey@gmail.com>
2022-08-26 09:58:58 +0100

    Reveal elements while scrolling

--- script.js
+++ script.js
@@ -183,6 +183,32 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 }); // constructor function
 headerObserver.observe(header);
 
+///////////// REVEAL Hidden Sections /////////////
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) {
+    return;
+  }
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: [0.15],
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 // ///////////////// DOM the Traversing ///////////////
 
 // const h1 = document.querySelector('h1');
